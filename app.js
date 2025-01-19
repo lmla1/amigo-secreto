3bb9154831c4d0096eb0d815c932965b29e19c81
Luis Manuel <luis.lopez2m1w@gmail.com>
2025-01-18 22:02:17 -0600

    update app.js

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+let amigos = [];
 
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo');
+    let nombreDeAmigo = amigo.value;
+
+    if (nombreDeAmigo == "") {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+
+    amigos.push(nombreDeAmigo);
+
+    let listaDeAmigos = document.getElementById('listaAmigos');
+    const li = document.createElement('li');
+    li.innerHTML = nombreDeAmigo;
+    listaDeAmigos.appendChild(li);
+
+    amigo.value = "";
+    amigo.focus();
+}
+
+function sortearAmigo() {
+    if (amigos.length > 0) {
+        let indiceRandom = Math.floor(Math.random() * amigos.length);
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = amigos[indiceRandom];
+    }
+}
